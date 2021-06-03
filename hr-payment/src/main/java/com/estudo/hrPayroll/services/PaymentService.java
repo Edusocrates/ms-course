4a32cebe6de5d87d3bf7467647ab19f88a1a3d91
Eduardo Socrates <edu5390@gmail.com>
2021-06-03 16:05:50 -0300

    Spring Cloud Feign Client

--- hr-payment/src/main/java/com/estudo/hrPayroll/services/PaymentService.java
+++ hr-payment/src/main/java/com/estudo/hrPayroll/services/PaymentService.java
@@ -2,6 +2,7 @@ package com.estudo.hrPayroll.services;
 
 import com.estudo.hrPayroll.entities.Payment;
 import com.estudo.hrPayroll.entities.Worker;
+import com.estudo.hrPayroll.feignClients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -14,18 +15,12 @@ import java.util.Map;
 public class PaymentService {
 
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClient workerFeignClient;
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
 
     public Payment getPayment( Long workerId, Integer days){
-        //utilizando o map para mapear a uri, e o HashMap que é uma classe composta igual o ArrayList
-        Map<String,String> uriVariables = new HashMap<>();
-        uriVariables.put("id",""+workerId);//insere os parametros os aspas converte para string dessa forma com +
 
-        //primeiro vem a uri que eu vou buscar e depois o tipo de retorno dela, depois vem os parametros
-        Worker worker = restTemplate.getForObject(workerHost+"workers/{id}",Worker.class,uriVariables);
+        Worker worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(),worker.getDailyIncome(),days);
     }
 
