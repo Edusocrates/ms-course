44b397eaf226c844325be1a82d1d3e973fd981e9
Eduardo Socrates <edu5390@gmail.com>
2021-06-07 16:25:08 -0300

    project hr-eureka Server created

---
+++ hr-eureka/src/test/java/com/estudo/hreurekaserver/HrEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
