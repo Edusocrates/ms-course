1c8bfc7a874f5dd876e05714ef852b991523b423
Eduardo Socrates <edu5390@gmail.com>
2021-06-11 16:20:22 -0300

    Hystrix Fault Tolerance

--- hr-payment/src/main/java/com/estudo/hrPayroll/HrPayrollApplication.java
+++ hr-payment/src/main/java/com/estudo/hrPayroll/HrPayrollApplication.java
@@ -2,12 +2,13 @@ package com.estudo.hrPayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @EnableEurekaClient
+@EnableCircuitBreaker
 @SpringBootApplication
 public class HrPayrollApplication {
 
