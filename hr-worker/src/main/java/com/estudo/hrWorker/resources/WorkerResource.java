b39655663c3e103b416b219449b9ed8e7ab26117
Eduardo Socrates <edu5390@gmail.com>
2021-06-14 20:47:25 -0300

    hr-worker as a config server Client(it's not working because of the bootstrap, fix later)

--- hr-worker/src/main/java/com/estudo/hrWorker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/estudo/hrWorker/resources/WorkerResource.java
@@ -5,6 +5,7 @@ import com.estudo.hrWorker.repositories.WorkerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +20,9 @@ import java.util.List;
 public class WorkerResource {
 
     private static Logger log = LoggerFactory.getLogger(WorkerResource.class);
+
+    @Value("${test.config}")
+    private String testConfig;
 
     @Autowired
     private WorkerRepository repository;
@@ -39,5 +43,11 @@ public class WorkerResource {
         Worker obj = repository.findById(id).get();//ou lança exception
         return ResponseEntity.ok(obj);
     }
+    @GetMapping(value = "/configs")
+    public ResponseEntity<List<Worker>> getConfigs(){
+        log.info("CONFIG ="+testConfig);
+        return ResponseEntity.noContent().build();
+    }
+
 
 }
