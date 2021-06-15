182f801d3db9637f5fa220df9d77c88c7871e710
Eduardo Socrates <edu5390@gmail.com>
2021-06-15 20:01:14 -0300

    Creating hr-user Project

---
+++ hr-user/src/test/java/com/estudo/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
